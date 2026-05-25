0a5839371d431e30a28684b141a0c36c4d036a1a
Sridhar <usridhar@thestackly.com>
2026-05-25 17:17:57 +0530

    .

---
+++ javaScript/contact.js
@@ -0,0 +1,56 @@
+// ================= MOBILE MENU =================
+
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+
+// ================= SCROLL ANIMATION =================
+
+const elements = document.querySelectorAll(
+  ".contact-form-container, .mini-card, .info-box, .map-box"
+);
+
+window.addEventListener("scroll", () => {
+
+  elements.forEach((element) => {
+
+    const elementTop = element.getBoundingClientRect().top;
+
+    if(elementTop < window.innerHeight - 100){
+
+      element.style.opacity = "1";
+      element.style.transform = "translateY(0px)";
+
+    }
+
+  });
+
+});
+
+
+// INITIAL STATE
+
+elements.forEach((element) => {
+
+  element.style.opacity = "0";
+  element.style.transform = "translateY(60px)";
+  element.style.transition = "0.8s ease";
+
+});
+
+
+// ================= HERO PARALLAX =================
+
+window.addEventListener("scroll", () => {
+
+  const hero = document.querySelector(".contact-hero");
+
+  let scrollPosition = window.pageYOffset;
+
+  hero.style.backgroundPositionY = scrollPosition * 0.5 + "px";
+
+});
